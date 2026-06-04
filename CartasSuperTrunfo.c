fe073e9dd6729bfded4672d6ad4ca86d0218fc68
programacaofda <202601849247@alunos.estacio.br>
2026-06-04 03:30:23 +0000

    Desafio novato concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,36 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-
+    char estado1;
+    char Codigo1[4];
+    char nomeCidade1[50];
+    int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
   // Área para entrada de dados
-
+    printf("Digite os dados da carta 1\n");
   // Área para exibição dos dados da cidade
+    printf("Estado (A-H): ");
+    scanf("%s", &estado1);
+    
+    printf("Código (ex: A01): ");
+    scanf("%s", &Codigo1);
+
+    printf("Nome da Cidade: ");
+    scanf("%s", &nomeCidade1);
+
+    printf("Populacao: ");
+    scanf("%d", &populacao1);
+
+    printf("Área: ");
+    scanf("%f", &area1);
+
+    printf("PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Número de Pontos turisticos: ");
+    scanf("%d", &pontosTuristicos1);
 
 return 0;
 } 
